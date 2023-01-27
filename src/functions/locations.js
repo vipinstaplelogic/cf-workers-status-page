16b880f16142aeb17ba840f108d23a34e95e77ba
VIPIN <75601676+vipinstaplelogic@users.noreply.github.com>
2023-01-27 22:48:36 +0530

    Update locations.js

--- src/functions/locations.js
+++ src/functions/locations.js
@@ -1,3 +1,15 @@
 export const locations = {
+  NRT: 'Tokyo',
+  KIX: 'Osaka',
+  BOM: 'Mumbai',
+  DEL: 'New Delhi',
+  SIN: 'Singapore',
+  SYD: 'Sydney',
+  MEL: 'Melbourne',
+  JNB: 'Johannesburg',
+  DXB: 'Dubai',
+  LHR: 'London',
+  CDG: 'Paris',
+  MAA: 'Chennai',
   MEL: 'Melbourne'
 }
